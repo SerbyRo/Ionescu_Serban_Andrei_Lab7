ec40b1c81343893f182b4de62456c45533dce252
SerbyRo <serbanandrei.ionescu@gmail.com>
2023-10-29 01:24:03 +0300

    Remove unnecesarry code

--- LibraryModel/Models/Customer.cs
+++ LibraryModel/Models/Customer.cs
@@ -1,11 +1,11 @@
 ﻿using LibraryModel.Models;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Ionescu_Serban_Andrei_Lab2.Models
 {
     public class Customer
     {
         public int CustomerID { get; set; }
-
         public int? CityID { get; set; }
         public City? City { get; set; }
         public string Name { get; set; }
